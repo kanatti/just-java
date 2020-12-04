9dcbd41309679a12128adbf40be619f0ee7f5376
kanatti <balukanattil@gmail.com>
2020-12-04 16:02:39 +0530

    Reverse polling logic

--- src/kanatti/justjava/networking/threads/BadPoller.java
+++ src/kanatti/justjava/networking/threads/BadPoller.java
@@ -3,9 +3,12 @@ package kanatti.justjava.networking.threads;
 import kanatti.justjava.networking.io.Streams;
 
 import javax.xml.bind.DatatypeConverter;
+import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BadPoller {
 
@@ -14,7 +17,7 @@ public class BadPoller {
     }
 
     public static class TimeBucket {
-        private final HashMap<Long, Integer> hits = new HashMap<>();
+        private final Map<Long, Integer> hits = new HashMap<>();
 
         public void addHit(long time) {
             int currentHits = this.hits.getOrDefault(time, 0);
@@ -27,7 +30,7 @@ public class BadPoller {
     }
 
     public static void main(String[] args) {
-        List<String> filenames = Arrays.asList("/tmp/data1.txt", "/tmp/data2.txt");
+        List<String> filenames = Arrays.asList("/tmp/data1.txt", "/tmp/data2.txt", "/tmp/data3.txt");
         for (String filename: filenames) {
             Streams.writeToTemp("Hello, World\nHow are you? " + filename, filename);
         }
@@ -39,18 +42,29 @@ public class BadPoller {
             threads[i].start();
         }
 
-        for(int i = 0; i < filenames.size(); i++) {
-            TimeBucket hits = new TimeBucket();
-            while (true) {
-                hits.addHit(getCurrentTenthMillis());
+        int polledCount = 0;
+
+        List<TimeBucket> hitsList = new ArrayList<>();
+        for (int i = 0; i < filenames.size(); i++) {
+            hitsList.add(new TimeBucket());
+        }
+
+        while (polledCount < threads.length) {
+            for(int i = 0; i < filenames.size(); i++) {
+                hitsList.get(i).addHit(getCurrentTenthMillis());
                 byte[] digest = threads[i].getDigest();
                 if (digest != null) {
                     String result = "Polled - " +  filenames.get(i) + " : " + DatatypeConverter.printHexBinary(digest);
                     System.out.println(result);
-                    break;
+                    polledCount +=1 ;
                 }
             }
-            System.out.println("Polling Status "  + hits.getStatus());
+        }
+
+        System.out.println("Polling Status:");
+        for (int i = 0; i < filenames.size(); i++) {
+            System.out.println(filenames.get(i));
+            System.out.println(hitsList.get(i).getStatus());
         }
     }
 
